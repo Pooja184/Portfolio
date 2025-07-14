2fb192f300ef5e053cc4b7175eccd23aa084c25e
pxxja <poojabhambid2004@gmail.com>
2025-07-14 23:33:09 +0530

    Create Scrollbar function

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -3,6 +3,11 @@ import { ToastContainer, toast } from 'react-toastify';
 import {Routes,Route} from 'react-router-dom'
 import Home from './pages/Home';
 import Navbar from './components/Navbar';
+import About from './pages/About';
+import Footer from './components/Footer';
+import Consultation from './components/Consultation';
+import ScrollToTop from './components/ScrollToTop';
+import Projects from './pages/Projects';
 
 
 const App = () => {
@@ -10,10 +15,14 @@ const App = () => {
     <div>
    <ToastContainer/>
      <Navbar/>
+     <ScrollToTop/>
      <Routes>
         <Route path='/' element={<Home/>}/>
-
+        <Route path='about' element={<About/>}/>
+        <Route path='/projects' element={<Projects/>}/>
      </Routes>
+     <Consultation/>
+     <Footer/>
     </div>
   )
 }
