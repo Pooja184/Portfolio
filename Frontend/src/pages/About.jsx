5eadc3cac46f26780f8f22997892563671a17873
pxxja <poojabhambid2004@gmail.com>
2025-11-05 13:56:50 +0530

    add actual text in work description instead of lorem

--- Frontend/src/pages/About.jsx
+++ Frontend/src/pages/About.jsx
@@ -55,15 +55,15 @@ const About = () => {
 
   const work=[
     {
-      description:"loremLorem ipsum dolor sit amet consectetur adipisicing elit. Maiores eius aperiam alias, sequi voluptatem repudiandae libero ad cumque? Perferendis modi sed iste libero? Mollitia quam ea excepturi, aut quas eveniet.",
+      description:"I don’t just do tasks because they’re assigned. I try to understand what’s really needed and why it matters.",
     },
      {
       description:
-        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores eius aperiam alias, sequi voluptatem repudiandae libero ad cumque? Perferendis modi sed iste libero? Mollitia quam ea excepturi, aut quas eveniet.",
+        "I focus on meaningful results that actually make an impact. I adapt quickly when things change and stay calm under pressure.",
     },
     {
       description:
-        " Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores eius aperiam alias, sequi voluptatem repudiandae libero ad cumque? Perferendis modi sed iste libero? Mollitia quam ea excepturi, aut quas eveniet.",
+        " I take feedback seriously and use it to improve fast. When situations get unclear or challenging, I don’t back off.  I stay reliable and steady. You can count on me to show up, take ownership, and get the work done the right way.",
     },
 
   ]
@@ -72,7 +72,7 @@ const About = () => {
     <div>
       <HeroPage text1={"About"} text2={"Hi, I'am Pooja"} />
       <Second
-        heading={"Building. Breaking. Learning. Growing — one line daily."}
+        heading={"Building. Breaking. Learning. Growing .  one line daily."}
         description={
           "I’m a developer who learns by doing — every project, every challenge, and every late-night bug fix helps me grow and get better at what I love. From designing smooth frontends to building solid backend systems, I enjoy working across the full stack. I mostly work with the MERN stack and take pride in writing clean, efficient, and maintainable code."
         }
